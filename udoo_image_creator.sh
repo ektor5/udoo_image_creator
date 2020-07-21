29f0241c64eb449676c032faca8b9bf92071e0b8
Tommaso Perondi <tommaso2001.pero@gmail.com>
2020-07-21 15:27:19 +0200

    Updated README, renamed main script

--- udoo_image_creator.sh
+++ udoo_image_creator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Script for the realization of the SDcard image and its configuration
-set -e
+#set -e
 
 # Included files
 DIR_MKUDOOBUNTU=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
@@ -30,6 +30,8 @@ function check_env() {
 PRINTS_DEBUG=1
 PRINTS_VERBOSE=1
 
+export MNTDIR=mnt
+export IMGSIZE=3
 # The first stage is the function that do the initial operation such as:
 # creating partitions, upload the bootloader, ...
 # Many of its operation are taken from ../include/imager.sh
@@ -46,36 +48,15 @@ function bootstrap() {
 
     echo_i "Starting setup"
 
-    # Create the empty image file - 4GB
-    echo_i "Creating the image file $OUTPUT..."
-    dd if=/dev/zero of=$OUTPUT bs=1 count=0 seek=3G 2>&1 > /dev/null
-    echo_ok "Image created!"
-    # Associate loop-device with .img file
-    losetup $LOOP $OUTPUT || echo_red "Cannot set $LOOP"
-
-    # Create the partitions - from include/imager.sh
-    create_partitions $OUTPUT $LOOP
-    # Copy the bootloader - from include/imager.sh
-    write_bootloader $OUTPUT $LOOP
-
-    # Mount udoobuntu18_04
-    mkdir -p mnt 2> /dev/null
-    mount "${LOOP}p1" mnt/
-    # Copy the kernel - from include/imager.sh
-    mkdir -p mnt/boot/
-    write_kernel $OUTPUT $LOOP
-
-    echo_ok "Setup complete!"
-
-    # Debootstrap
     echo_i "Starting debootstrap ..."
-    qemu-debootstrap --arch=armhf --verbose bionic mnt/ 2>&1 > out.log &
+    qemu-debootstrap --arch=armhf --verbose focal "$MNTDIR" 2>&1 > out.log &
     local process_pid=$!
     progress_bar $process_pid "first stage"
-    #tar -C mnt/ -xf ubuntu-base-18.04.3-base-armhf.tar
-    cp /usr/bin/qemu-arm-static mnt/usr/bin
-    echo_ok "debootstrap: Done!"
-
+    cp /usr/bin/qemu-arm-static "${MNTDIR}/usr/bin"
+    # Copy the kernel - from include/imager.sh
+    mkdir -p "${MNTDIR}/boot/"
+    write_kernel $MNTDIR
+    echo_ok "Bootstrap: DONE"
 }
 
 # This function configure the system: adding the default user, set the root
@@ -83,7 +64,7 @@ function bootstrap() {
 function configuration() {
     echo_i "Starting configuration..."
     # Edit the hostname file
-    chroot mnt/ /bin/bash -c "echo \"$HOSTNAME\" > /etc/hostname"
+    chroot "${MNTDIR}/" /bin/bash -c "echo \"$HOSTNAME\" > /etc/hostname"
 
     # Install packages - from include/packages.sh
     add_source_list >> out.log 2>&1 &
@@ -95,16 +76,18 @@ function configuration() {
     install_packages >> out.log 2>&1 &
     process_pid=$!
     progress_bar $process_pid "install packages"
-
+    
+    #Install firmwares
+    cp "source/firmware/"* "./${MNTDIR}/lib/firmware/ti-connectivity/"
     echo_i "Adding resizefs"
-    install -m 755 patches/firstrun  "mnt/etc/init.d"
-    chroot "mnt/" /bin/bash -c "update-rc.d firstrun defaults > /dev/null 2>&1"
-    cp patches/g_multi_setup.sh mnt/etc/rc.local
-    chmod +x mnt/etc/rc.local
+    install -m 755 patches/firstrun  "./${MNTDIR}/etc/init.d"
+    chroot "${MNTDIR}/" /bin/bash -c "update-rc.d firstrun defaults > /dev/null 2>&1"
+    cp patches/g_multi_setup.sh "./${MNTDIR}/etc/rc.local"
+    chmod +x "./${MNTDIR}/etc/rc.local"
 
     echo_i "Configuring Network ..."
-    cp patches/network_interface mnt/etc/network/interfaces
-
+    cp patches/network_interface "./${MNTDIR}/etc/netplan/01-network-manager-all.yaml"
+    cp patches/resolv.conf "${MNTDIR}/etc/resolv.conf"
     # Setup the user and root - from include/set_user_and_root.sh
     set_root
     set_user
@@ -112,33 +95,49 @@ function configuration() {
     echo_ok "Configuration complete!"
 }
 
+function create_image(){
+    # Create the empty image file - 4GB
+    echo_i "Creating the image file $OUTPUT..."
+    dd if=/dev/zero of=$OUTPUT bs=1 count=0 seek="${IMGSIZE}G" 2>&1 > /dev/null
+    echo_ok "Image created!"
+    # Associate loop-device with .img file
+    losetup $LOOP $OUTPUT || echo_red "Cannot set $LOOP"
+    # Create the partitions - from include/imager.sh
+    create_partitions $OUTPUT $LOOP
+    # Copy the bootloader - from include/imager.sh
+    write_bootloader $OUTPUT $LOOP
+    echo_e "$LOOP $MNTDIR"
+    mount "${LOOP}p1" $MNTDIR
+    read -p "Press any key to resume ..."
+}
 
 function clean() {
     # Read arguments
     local LOOP=$1
 
-    if [ -d mnt ]
+    if [ -d $MNTDIR ]
     then
-        umount -lf mnt
-        rm -rf mnt
+        umount  $MNTDIR
+        rm -rf $MNTDIR
     fi
     losetup -d "$LOOP"
     sync
 
-    echo_green "Cleaned successfully"
+    echo_ok "Cleaned successfully"
 }
 
 ################################################################################
 function main() {
-    checkroot
+    #checkroot
     # Configuration
-    local OUTPUT="udoobuntu-udoo_neo-18.04_$(date +%Y%m%d-%H%M).img"
+    local OUTPUT="udoobuntu-udoo_neo-20.04_$(date +%Y%m%d-%H%M).img"
     local LOOP=$(losetup -f)
 
     echo_i "Check dependencies..."
     check_env
     check_dependencies "debootstrap"
     check_dependencies "qemu-arm-static"
+    create_image
 
     trap "clean $LOOP" INT TERM KILL
 
